3a2f34069545007a7b8ebc0e60167b8aa2475f7f
lewiskk016 <lewiskk016@gmail.com>
2023-06-11 14:18:10 -0400

    finish user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -3,19 +3,19 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     email: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     },
     hashedPassword: {
-        type: String,
-        required: true
+      type: String,
+      required: true
     }
-}, {
+  }, {
     timestamps: true
-});
+  });
 
-module.exports = mongoose.model('User', userSchema);
+  module.exports = mongoose.model('User', userSchema);
